c66a9dd700442205807435d8a8466d47b8c0a359
Yu Jinyan <yujinyan@outlook.com>
2022-05-08 00:17:51 +0800

    init work on home page

---
+++ src/@lekoarts/gatsby-theme-emilia/assets/bg-pattern.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+import {StaticImage} from "gatsby-plugin-image"
+
+const BGPattern = () => (
+    <StaticImage
+        src="./hogwarts-spring.png"
+        // objectFit="cover"
+        objectPosition="center"
+    />
+)
+
+export default BGPattern
